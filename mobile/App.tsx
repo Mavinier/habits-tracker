8790ffce499dbd744cbd0242a48bf0afd20ca66f
Marlon Mavinier <mavinier2010@gmail.com>
2023-01-20 18:13:33 -0300

    eslint config mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,30 +1,37 @@
+/* eslint-disable camelcase */
 import { StatusBar } from 'react-native';
+
 import {
-  useFonts, 
-  Inter_400Regular, 
-  Inter_600SemiBold, 
-  Inter_700Bold, 
-  Inter_800ExtraBold
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
 } from '@expo-google-fonts/inter';
+
 import { Loading } from './src/components/Loading/Loading';
 import { Home } from './src/screens/Home';
 
 export default function App() {
- const [ fontsLoaded ] = useFonts({
-    Inter_400Regular, 
-    Inter_600SemiBold, 
-    Inter_700Bold, 
-    Inter_800ExtraBold
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
   });
-  
+
   if (!fontsLoaded) {
-   return (<Loading />);
-  };
+    return <Loading />;
+  }
 
   return (
     <>
       <Home />
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
     </>
   );
-};
+}
--- mobile/src/components/Header/Header.tsx
+++ mobile/src/components/Header/Header.tsx
@@ -1,15 +1,16 @@
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, TouchableOpacity, Text } from 'react-native';
+
 import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
 
 import Logo from '../../assets/logo.svg';
 
 export function Header() {
-  return(
+  return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
 
-      <TouchableOpacity 
+      <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
       >
@@ -17,7 +18,6 @@ export function Header() {
 
         <Text className="text-white ml-3 font-semibold text-base">Novo</Text>
       </TouchableOpacity>
-
     </View>
-  )
+  );
 }
